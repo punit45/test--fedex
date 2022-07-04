f88f5a38981db8bdb9cd156de15fcc6f89383142
punit45 <ipunit96@gmail.com>
2022-07-04 15:18:53 +0530

    test for fedex

---
+++ index.js
@@ -0,0 +1,31 @@
+// first we will select all the class and then the sorting .  sort().. 
+
+// const arr = ["a","b","c","d","e","f","g","h","i"];
+
+// const newAarr = arr.sort();
+
+// console.log(newAarr);  
+
+
+// above returns the sorted items . 
+
+
+
+// // we have taken an array 
+// // sort out the array 
+// // then we will pass the new array to the submit . 
+
+// const newArr1 = arr.random();
+// console.log(newArr1);
+
+
+let arr = [1,2,3,4,5];
+
+ function(array){
+    let newArr = array.length;
+    let newAarr = random.Index;
+
+    while( newAarr)
+
+ }
+
